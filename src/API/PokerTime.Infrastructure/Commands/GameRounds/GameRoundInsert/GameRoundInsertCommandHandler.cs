2b8fcdea18e57a5f38e21bdb7e04ec282ece02bd
StefanFanaru <stefan.fanaru@outlook.com>
2023-10-26 19:09:06 +0300

    Transactional round insert

--- src/API/PokerTime.Infrastructure/Commands/GameRounds/GameRoundInsert/GameRoundInsertCommandHandler.cs
+++ src/API/PokerTime.Infrastructure/Commands/GameRounds/GameRoundInsert/GameRoundInsertCommandHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using PokerTime.Core.Abstractions;
@@ -25,64 +24,64 @@ public class GameRoundInsertCommandHandler : IRequestHandler<GameRoundInsertComm
     public async Task<IOperationResult<GameRoundInsertCommandResponse>> Handle(GameRoundInsertCommand request,
         CancellationToken cancellationToken)
     {
-        var query = _repository.Query<GameRound>()
-            .Where(x => x.GameId == request.GameId.ToString())
-            .Where(x => x.WorkItemId == request.WorkItemId);
-        var roundId = await query
-            .Select(x => x.Id)
-            .FirstOrDefaultAsync(cancellationToken);
-        var gamePlayersIds = await _repository.Query<GameRoundPlayer>()
-            .Where(r => r.Round.GameId == request.GameId.ToString())
-            .Where(x => x.Round.Game.Status != GameStatus.Ended)
-            .Select(x => x.PlayerId)
-            .Distinct()
-            .ToListAsync(cancellationToken);
-
-        var activePlayersIds = gamePlayersIds
-            .Where(x => _signalRConnectionManager.IsUserConnectedToGame(x, request.GameId.ToString())).ToList();
-
-        if (!activePlayersIds.Contains(request.User.Id))
+        var result = new GameRoundInsertCommandResponse();
+        await _repository.ExecuteTransactionalAsync(async () =>
         {
-            activePlayersIds.Add(request.User.Id);
-        }
+            var query = _repository.Query<GameRound>()
+                .Where(x => x.GameId == request.GameId.ToString())
+                .Where(x => x.WorkItemId == request.WorkItemId);
+            var roundId = await _repository.Query<GameRound>()
+                .Where(x => x.GameId == request.GameId.ToString())
+                .Where(x => x.WorkItemId == request.WorkItemId)
+                .Select(x => x.Id)
+                .FirstOrDefaultAsync(cancellationToken);
+            var gamePlayersIds = await _repository.Query<GameRoundPlayer>()
+                .Where(r => r.Round.GameId == request.GameId.ToString())
+                .Where(x => x.Round.Game.Status != GameStatus.Ended)
+                .Select(x => x.PlayerId)
+                .Distinct()
+                .ToListAsync(cancellationToken);
 
-        if (!string.IsNullOrWhiteSpace(roundId))
-        {
-            if (!await query.AnyAsync(x => x.Players.Any(p => p.PlayerId == request.User.Id), cancellationToken))
+            var activePlayersIds = gamePlayersIds
+                .Where(x => _signalRConnectionManager.IsUserConnectedToGame(x, request.GameId.ToString())).ToList();
+
+            if (!activePlayersIds.Contains(request.User.Id))
             {
-                await InsertRoundPlayer(roundId, request.User.Id);
+                activePlayersIds.Add(request.User.Id);
             }
 
-            var result = await query.Select(x => new GameRoundInsertCommandResponse
+            if (!string.IsNullOrWhiteSpace(roundId))
             {
-                RoundId = x.Id,
-                CardsWereFlipped = x.CardsWereFlipped,
-                SubmittedStoryPoints = x.SubmittedStoryPoints,
-                WorkItemId = x.WorkItemId,
-                ActivePlayersIds = activePlayersIds
-            }).FirstOrDefaultAsync(cancellationToken);
-            return ResultBuilder.Ok(result);
-        }
+                if (!await query.AnyAsync(x => x.Players.Any(p => p.PlayerId == request.User.Id), cancellationToken))
+                {
+                    await InsertRoundPlayer(roundId, request.User.Id);
+                }
 
-        var gameRound = new GameRound(request.GameId.ToString(), request.WorkItemId);
-        try
-        {
+                result = await query.Select(x => new GameRoundInsertCommandResponse
+                {
+                    RoundId = x.Id,
+                    CardsWereFlipped = x.CardsWereFlipped,
+                    SubmittedStoryPoints = x.SubmittedStoryPoints,
+                    WorkItemId = x.WorkItemId,
+                    ActivePlayersIds = activePlayersIds
+                }).SingleAsync(cancellationToken);
+                return;
+            }
+
+            var gameRound = new GameRound(request.GameId.ToString(), request.WorkItemId);
             _repository.Insert(gameRound);
             await _repository.SaveAsync(cancellationToken);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
+            await InsertRoundPlayer(gameRound.Id, request.User.Id);
 
-        await InsertRoundPlayer(gameRound.Id, request.User.Id);
-
-        return ResultBuilder.Ok(new GameRoundInsertCommandResponse
-        {
-            RoundId = gameRound.Id,
-            WorkItemId = gameRound.WorkItemId,
-            ActivePlayersIds = activePlayersIds
+            result = new GameRoundInsertCommandResponse
+            {
+                RoundId = gameRound.Id,
+                WorkItemId = gameRound.WorkItemId,
+                ActivePlayersIds = activePlayersIds
+            };
         });
+
+        return ResultBuilder.Ok(result);
     }
 
     private async Task InsertRoundPlayer(string roundId, string playerId)
